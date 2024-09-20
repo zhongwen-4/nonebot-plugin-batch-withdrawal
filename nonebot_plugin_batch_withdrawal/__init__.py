9ed3d7efaca53f2b8b0e9fdfea7622d4516b9885
zhongwen-4 <2401128923@qq.com>
2024-09-20 11:29:32 +0800

    更新数据写入方式为aiosqlite

--- nonebot_plugin_batch_withdrawal/__init__.py
+++ nonebot_plugin_batch_withdrawal/__init__.py
@@ -1,20 +1,8 @@
-import sqlite3
+import aiosqlite
 
 from nonebot import on_message, get_driver, require, logger
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, GROUP_ADMIN, GROUP_OWNER
 from nonebot.permission import SUPERUSER
-from nonebot.plugin import PluginMetadata
-
-
-__plugin_meta__ = PluginMetadata(
-    name= "批量撤回",
-    description="批量撤回消息",
-    usage="发送 [delete <@qq> 数量]",
-    supported_adapters= {"~onebot.v11"},
-    type= "application",
-    homepage= "https://github.com/zhongwen-4/nonebot-plugin-batch-withdrawal"
-)
-
 
 require("nonebot_plugin_localstore")
 require("nonebot_plugin_alconna")
@@ -29,22 +17,13 @@ delete = on_alconna(Alconna("delete", Args["qq?", At]["num", int]), permission= 
 
 @driver.on_startup
 async def _():
-    global conn, cursur
-    conn = sqlite3.connect(f"{path}/message_id.db")
-    logger.debug("[message_id] 数据库连接成功")
     logger.info(f"获取数据目录成功 -> {path}")
-    cursur = conn.cursor()
-
-@driver.on_shutdown
-async def shutdown_handle():
-    logger.debug("[message_id] 数据库关闭")
-    conn.commit()
-    conn.close()
 
 @write_message_id.handle()
 async def write_message_id_handle(bot: Bot, event: GroupMessageEvent):
-    cursur.execute(f"CREATE TABLE IF NOT EXISTS _{event.group_id}(id INTEGER PRIMARY KEY AUTOINCREMENT, message_id INTEGER, user_id INTEGER)")
-    conn.commit()
+    async with aiosqlite.connect(f'{path}/message_id.db') as db:
+        await db.execute(f"CREATE TABLE IF NOT EXISTS _{event.group_id}(id INTEGER PRIMARY KEY AUTOINCREMENT, message_id INTEGER, user_id INTEGER)")
+        await db.commit()
 
     role = await bot.get_group_member_info(group_id=event.group_id, user_id= event.self_id)
 
@@ -54,16 +33,18 @@ async def write_message_id_handle(bot: Bot, event: GroupMessageEvent):
 
     if role["role"] == "owner":
         if event.sender.role == "member" or "admin":
-            cursur.execute(f"INSERT INTO _{event.group_id}(message_id, user_id) VALUES(?,?)", (event.message_id, event.user_id))
-            conn.commit()
+            async with aiosqlite.connect(f'{path}/message_id.db') as db:
+                await db.execute(f"INSERT INTO _{event.group_id}(message_id, user_id) VALUES(?,?)", (event.message_id, event.user_id))
+                await db.commit()
             logger.debug(f"数据库 [message_id] | _{event.group_id} 写入 -> 消息ID: {event.message_id}")
         else:
             logger.debug("消息对象是群主, 跳过写入")
     
     else:
         if event.sender.role == "member":
-            cursur.execute(f"INSERT INTO _{event.group_id}(message_id, user_id) VALUES(?,?)", (event.message_id, event.user_id))
-            conn.commit()
+            async with aiosqlite.connect(f'{path}/message_id.db') as db:
+                await db.execute(f"INSERT INTO _{event.group_id}(message_id, user_id) VALUES(?,?)", (event.message_id, event.user_id))
+                await db.commit()
             logger.debug(f"数据库 [message_id] | _{event.group_id} 写入 -> 消息ID: {event.message_id}")
         else:
             logger.debug("消息对象是管理员/群主, 跳过写入")
@@ -77,21 +58,26 @@ async def delete_handle(bot: Bot, event: GroupMessageEvent, rum: Arparma, qq: Ma
     if qq.available:
         if isinstance(qq.result, At):
             user_id = qq.result.target
-            cursur.execute(f"SELECT message_id FROM _{event.group_id} WHERE user_id= ? ORDER BY message_id DESC LIMIT ?", (user_id, rum.all_matched_args["num"]))
-            result = cursur.fetchall()
-            for i in result:
-                await bot.delete_msg(message_id= i[0])
-                cursur.execute(f"DELETE FROM _{event.group_id} WHERE message_id = ?", (i[0],))
-                conn.commit()
-                logger.debug(f"数据库 [message_id] | _{event.group_id} 删除 -> 消息ID: {i[0]}")
-            await delete.finish(f"成功撤回{user_id} 的 {rum.all_matched_args['num']} 条消息", reply_message = True)
+
+            async with aiosqlite.connect(f'{path}/message_id.db') as db:
+                async with db.execute(f"SELECT message_id FROM _{event.group_id} WHERE user_id= ? ORDER BY message_id DESC LIMIT ?", (user_id, rum.all_matched_args["num"])) as cursur:
+                    result = await cursur.fetchall()
+                    for i in result:
+                        await bot.delete_msg(message_id= i[0])
+                        async with aiosqlite.connect(f'{path}/message_id.db') as db:
+                            await db.execute(f"DELETE FROM _{event.group_id} WHERE message_id = ?", (i[0],))
+                            await db.commit()
+                            logger.debug(f"数据库 [message_id] | _{event.group_id} 删除 -> 消息ID: {i[0]}")
+                    await delete.finish(f"成功撤回{user_id} 的 {rum.all_matched_args['num']} 条消息", reply_message = True)
     
     else:
-        cursur.execute(f"SELECT message_id FROM _{event.group_id} ORDER BY id DESC LIMIT ?", (rum.all_matched_args["num"],))
-        result = cursur.fetchall()
-        for i in result:
-            await bot.delete_msg(message_id=i[0])
-            cursur.execute(f"DELETE FROM _{event.group_id} WHERE message_id = ?", (i[0],))
-            conn.commit()
-            logger.debug(f"数据库 [message_id] | _{event.group_id} 删除 -> 消息ID: {i[0]}")
+        async with aiosqlite.connect(f'{path}/message_id.db') as db:
+            async with db.execute(f"SELECT message_id FROM _{event.group_id} ORDER BY id DESC LIMIT ?", (rum.all_matched_args["num"],)) as cursur:
+                result = await cursur.fetchall()
+                for i in result:
+                    await bot.delete_msg(message_id=i[0])
+                    async with aiosqlite.connect(f'{path}/message_id.db') as db:
+                        await db.execute(f"DELETE FROM _{event.group_id} WHERE message_id = ?", (i[0],))
+                        await db.commit()
+                        logger.debug(f"数据库 [message_id] | _{event.group_id} 删除 -> 消息ID: {i[0]}")
         await delete.finish(f"成功撤回 {rum.all_matched_args['num']} 条消息", reply_message = True)
